3cb09bec38221eabef7ca019e00ac7606e00910b
Jeroen Van Der Donckt <boebievdd@gmail.com>
2022-11-25 16:31:46 +0100

    :robot: start of Python CI-CD

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    rust_extensions=[RustExtension("tsdownsample._rust.tsdownsample_rs", binding=Binding.PyO3)],
+    rust_extensions=[
+        RustExtension("tsdownsample._rust.tsdownsample_rs", binding=Binding.PyO3)
+    ],
 )
--- tsdownsample/downsampling_interface.py
+++ tsdownsample/downsampling_interface.py
@@ -1,14 +1,13 @@
+import warnings
+from abc import ABC, abstractmethod
 from types import ModuleType
 from typing import Callable
+
 import numpy as np
 import pandas as pd
 
-from abc import ABC, abstractmethod
-
-DOWNSAMPLE_F = 'downsample'
 
 class DownsampleInterface(ABC):
-
     def __init__(self, name: str) -> None:
         self.name = name
 
@@ -16,19 +15,20 @@ class DownsampleInterface(ABC):
     def _construct_output_series(s: pd.Series, idxs: np.ndarray) -> pd.Series:
         return s.iloc[idxs]
 
-    def _supports_dtype(self, s: pd.Series):
-        # base case
-        if self.dtype_regex_list is None:
-            return
+    # def _supports_dtype(self, s: pd.Series):
+    #     # base case
+    #     if self.dtype_regex_list is None:
+    #         return
 
-        for dtype_regex_str in self.dtype_regex_list:
-            m = re.compile(dtype_regex_str).match(str(s.dtype))
-            if m is not None:  # a match is found
-                return
-        raise ValueError(
-            f"{s.dtype} doesn't match with any regex in {self.dtype_regex_list}"
-        )
+    #     for dtype_regex_str in self.dtype_regex_list:
+    #         m = re.compile(dtype_regex_str).match(str(s.dtype))
+    #         if m is not None:  # a match is found
+    #             return
+    #     raise ValueError(
+    #         f"{s.dtype} doesn't match with any regex in {self.dtype_regex_list}"
+    #     )
 
+    @abstractmethod
     def downsample(self, s: pd.Series, n_out: int, parallel: bool = False) -> pd.Series:
         """Downsample a pandas series to n_out samples.
 
@@ -40,22 +40,27 @@ class DownsampleInterface(ABC):
             The number of samples to downsample to.
         parallel : bool, optional
             Whether to use parallel processing (if available), by default False.
-        
+
         Returns
         -------
         pd.Series
             The downsampled series.
         """
         raise NotImplementedError
-    
+
     def __repr__(self) -> str:
         return f"{self.name}"
 
-# ------------------- Rust Downsample Interface -------------------
 
-def _switch_mod_with_y(y_dtype: np.dtype, mod: ModuleType, downsample_func: str = DOWNSAMPLE_F):
+# ------------------- Rust Downsample Interface -------------------
+DOWNSAMPLE_F = "downsample"
+
+
+def _switch_mod_with_y(
+    y_dtype: np.dtype, mod: ModuleType, downsample_func: str = DOWNSAMPLE_F
+) -> Callable:
     """The x-data is not considered in the downsampling
-    
+
     Assumes equal binning.
 
     Parameters
@@ -70,36 +75,39 @@ def _switch_mod_with_y(y_dtype: np.dtype, mod: ModuleType, downsample_func: str 
     # FLOATS
     if np.issubdtype(y_dtype, np.floating):
         if y_dtype == np.float16:
-            return getattr(mod, downsample_func + '_f16')
+            return getattr(mod, downsample_func + "_f16")
         elif y_dtype == np.float32:
-            return getattr(mod, downsample_func + '_f32')
+            return getattr(mod, downsample_func + "_f32")
         elif y_dtype == np.float64:
-            return getattr(mod, downsample_func + '_f64')
+            return getattr(mod, downsample_func + "_f64")
     # INTS
     elif np.issubdtype(y_dtype, np.integer):
         if y_dtype == np.int16:
-            return getattr(mod, downsample_func + '_i16')
+            return getattr(mod, downsample_func + "_i16")
         elif y_dtype == np.int32:
-            return getattr(mod, downsample_func + '_i32')
+            return getattr(mod, downsample_func + "_i32")
         elif y_dtype == np.int64:
-            return getattr(mod, downsample_func + '_i64')
+            return getattr(mod, downsample_func + "_i64")
     # UINTS
     elif np.issubdtype(y_dtype, np.unsignedinteger):
         if y_dtype == np.uint16:
-            return getattr(mod, downsample_func + '_u16')
+            return getattr(mod, downsample_func + "_u16")
         elif y_dtype == np.uint32:
-            return getattr(mod, downsample_func + '_u32')
+            return getattr(mod, downsample_func + "_u32")
         elif y_dtype == np.uint64:
-            return getattr(mod, downsample_func + '_u64')
+            return getattr(mod, downsample_func + "_u64")
     # BOOLS
     # TODO: support bools
     # elif data_dtype == np.bool:
-        # return mod.downsample_bool
+    # return mod.downsample_bool
     raise ValueError(f"Unsupported data type (for y): {y_dtype}")
 
-def _switch_mod_with_x_and_y(x_dtype: np.dtype, y_dtype: np.dtype, mod: ModuleType):
+
+def _switch_mod_with_x_and_y(
+    x_dtype: np.dtype, y_dtype: np.dtype, mod: ModuleType
+) -> Callable:
     """The x-data is considered in the downsampling
-    
+
     Assumes equal binning.
 
     Parameters
@@ -114,35 +122,35 @@ def _switch_mod_with_x_and_y(x_dtype: np.dtype, y_dtype: np.dtype, mod: ModuleTy
     # FLOATS
     if np.issubdtype(x_dtype, np.floating):
         if x_dtype == np.float16:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_f16')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_f16")
         elif x_dtype == np.float32:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_f32')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_f32")
         elif x_dtype == np.float64:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_f64')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_f64")
     # INTS
     elif np.issubdtype(x_dtype, np.integer):
         if x_dtype == np.int16:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_i16')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_i16")
         elif x_dtype == np.int32:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_i32')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_i32")
         elif x_dtype == np.int64:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_i64')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_i64")
     # UINTS
     elif np.issubdtype(x_dtype, np.unsignedinteger):
         if x_dtype == np.uint16:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_u16')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_u16")
         elif x_dtype == np.uint32:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_u32')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_u32")
         elif x_dtype == np.uint64:
-            return _switch_mod_with_y(y_dtype, mod, f'{DOWNSAMPLE_F}_u64')
+            return _switch_mod_with_y(y_dtype, mod, f"{DOWNSAMPLE_F}_u64")
     # BOOLS
     # TODO: support bools
     # elif data_dtype == np.bool:
-        # return mod.downsample_bool
+    # return mod.downsample_bool
     raise ValueError(f"Unsupported data type (for x): {x_dtype}")
 
-class RustDownsamplingInterface(DownsampleInterface):
 
+class RustDownsamplingInterface(DownsampleInterface):
     def __init__(self, name: str, resampling_mod: ModuleType) -> None:
         super().__init__(name + " [tsdownsample_rs]")
         self.rust_mod = resampling_mod
@@ -161,25 +169,44 @@ class RustDownsamplingInterface(DownsampleInterface):
         elif hasattr(self.rust_mod, "scalar_parallel"):
             # use scalar implementation if available (when no SIMD available)
             self.mod_multi_core = self.rust_mod.scalar_parallel
-        
-        
+
     def _downsample_without_x(self, s: pd.Series, n_out: int) -> pd.Series:
         downsample_method = _switch_mod_with_y(s.dtype, self.mod_single_core)
         idxs = downsample_method(s.values, n_out)
         return self._construct_output_series(s, idxs)
-    
+
     def _downsample_with_x(self, s: pd.Series, n_out: int) -> pd.Series:
-        downsample_method = _switch_mod_with_x_and_y(s.index.dtype, s.dtype, self.mod_single_core)
+        downsample_method = _switch_mod_with_x_and_y(
+            s.index.dtype, s.dtype, self.mod_single_core
+        )
         idxs = downsample_method(s.index.values, s.values, n_out)
         return self._construct_output_series(s, idxs)
 
     def _downsample_without_x_parallel(self, s: pd.Series, n_out: int) -> pd.Series:
-        downsample_method = _switch_mod_with_y(s.dtype, self.mod_multi_core)
+        if self.mod_multi_core is not None:
+            downsample_method = _switch_mod_with_y(s.dtype, self.mod_multi_core)
+        else:
+            warnings.warn(
+                "No multi-core implementation available. "
+                "Falling back to single core implementation."
+            )
+            downsample_method = _switch_mod_with_y(s.dtype, self.mod_single_core)
         idxs = downsample_method(s.values, n_out)
         return self._construct_output_series(s, idxs)
-    
+
     def _downsample_with_x_parallel(self, s: pd.Series, n_out: int) -> pd.Series:
-        downsample_method = _switch_mod_with_x_and_y(s.index.dtype, s.dtype, self.mod_multi_core)
+        if self.mod_multi_core is not None:
+            downsample_method = _switch_mod_with_x_and_y(
+                s.index.dtype, s.dtype, self.mod_multi_core
+            )
+        else:
+            warnings.warn(
+                "No multi-core implementation available. "
+                "Falling back to single core implementation."
+            )
+            downsample_method = _switch_mod_with_x_and_y(
+                s.index.dtype, s.dtype, self.mod_single_core
+            )
         idxs = downsample_method(s.index.values, s.values, n_out)
         return self._construct_output_series(s, idxs)
 
@@ -198,10 +225,11 @@ class RustDownsamplingInterface(DownsampleInterface):
             else:
                 return self._downsample_with_x(s, n_out)
 
-# ------------------ Numpy Downsample Interface ------------------ 
+
+# ------------------ Numpy Downsample Interface ------------------
+
 
 class FuncDownsamplingInterface(DownsampleInterface):
-
     def __init__(self, name: str, downsample_func: Callable) -> None:
         super().__init__("[Func]_" + name)
         self.downsample_func = downsample_func
