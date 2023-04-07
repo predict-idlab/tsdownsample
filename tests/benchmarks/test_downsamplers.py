a1bf884eb49ea7c578904d96d67e0f93833d1301
jvdd <boebievdd@gmail.com>
2023-04-07 14:09:37 +0200

    :recycle: only bench 5M

--- tests/benchmarks/test_downsamplers.py
+++ tests/benchmarks/test_downsamplers.py
@@ -8,7 +8,7 @@ from tsdownsample import (
     MinMaxLTTBDownsampler,
 )
 
-NB_SAMPLES = ["5,000,000", "50,000,000"]
+NB_SAMPLES = ["5,000,000"]  # , "50,000,000"]
 N_OUT = ["100", "1,000", "5,000"]
 Y_DTYPES = [np.float32, np.float64] + [np.int32, np.int64]
 
