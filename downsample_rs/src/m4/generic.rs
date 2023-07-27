3442ca68b79b338fb2b3ff7c6ef462c8a5ae476b
Niels Praet <niels.praet001@gmail.com>
2023-07-27 10:02:14 +0200

    ♻️ refactor: use slices in m4 generic

--- downsample_rs/src/m4/generic.rs
+++ downsample_rs/src/m4/generic.rs
@@ -14,20 +14,20 @@ use rayon::prelude::*;
 
 #[inline(always)]
 pub(crate) fn m4_generic<T: Copy + PartialOrd>(
-    arr: ArrayView1<T>,
+    arr: &[T],
     n_out: usize,
-    f_argminmax: fn(ArrayView1<T>) -> (usize, usize),
-) -> Array1<usize> {
+    f_argminmax: fn(&[T]) -> (usize, usize),
+) -> Vec<usize> {
     // Assumes n_out is a multiple of 4
     if n_out >= arr.len() {
-        return Array1::from((0..arr.len()).collect::<Vec<usize>>());
+        return (0..arr.len()).collect();
     }
 
     // arr.len() - 1 is used to match the delta of a range-index (0..arr.len()-1)
     let block_size: f64 = (arr.len() - 1) as f64 / (n_out / 4) as f64;
     let arr_ptr = arr.as_ptr();
 
-    let mut sampled_indices: Array1<usize> = Array1::<usize>::default(n_out);
+    let mut sampled_indices: Vec<usize> = vec![usize::default(); n_out];
 
     let mut start_idx: usize = 0;
     for i in 0..n_out / 4 {
@@ -36,9 +36,7 @@ pub(crate) fn m4_generic<T: Copy + PartialOrd>(
         let end: f64 = block_size * (i + 1) as f64;
         let end_idx: usize = end as usize + 1;
 
-        let (min_index, max_index) = f_argminmax(unsafe {
-            ArrayView1::from_shape_ptr((end_idx - start_idx,), arr_ptr.add(start_idx))
-        });
+        let (min_index, max_index) = f_argminmax(&arr[start_idx..end_idx]);
 
         // Add the indexes in sorted order
         sampled_indices[4 * i] = start_idx;
@@ -59,21 +57,21 @@ pub(crate) fn m4_generic<T: Copy + PartialOrd>(
 
 #[inline(always)]
 pub(crate) fn m4_generic_parallel<T: Copy + PartialOrd + Send + Sync>(
-    arr: ArrayView1<T>,
+    arr: &[T],
     n_out: usize,
     n_threads: usize,
-    f_argminmax: fn(ArrayView1<T>) -> (usize, usize),
-) -> Array1<usize> {
+    f_argminmax: fn(&[T]) -> (usize, usize),
+) -> Vec<usize> {
     // Assumes n_out is a multiple of 4
     if n_out >= arr.len() {
-        return Array1::from((0..arr.len()).collect::<Vec<usize>>());
+        return (0..arr.len()).collect::<Vec<usize>>();
     }
 
     // arr.len() - 1 is used to match the delta of a range-index (0..arr.len()-1)
     let block_size: f64 = (arr.len() - 1) as f64 / (n_out / 4) as f64;
 
     // Store the enumerated indexes in the output array
-    let mut sampled_indices: Array1<usize> = Array1::from_vec((0..n_out).collect::<Vec<usize>>());
+    let mut sampled_indices: Vec<usize> = Vec::with_capacity(n_out);
 
     // to limit the amounts of threads Rayon uses, an explicit threadpool needs to be created
     // in which the required code is "installed". This limits the amount of used threads.
@@ -82,27 +80,28 @@ pub(crate) fn m4_generic_parallel<T: Copy + PartialOrd + Send + Sync>(
         .num_threads(n_threads)
         .build();
 
+    // TODO: remove ndarray dependency from this
     let zip_func = || {
-        Zip::from(sampled_indices.exact_chunks_mut(4)).par_for_each(|mut sampled_index| {
-            let i: f64 = unsafe { *sampled_index.uget(0) >> 2 } as f64;
-            let start_idx: usize = (block_size * i) as usize + (i != 0.0) as usize;
-            let end_idx: usize = (block_size * (i + 1.0)) as usize + 1;
+        Zip::from(Array1::from(sampled_indices).exact_chunks_mut(4)).par_for_each(
+            |mut sampled_index| {
+                let i: f64 = unsafe { *sampled_index.uget(0) >> 2 } as f64;
+                let start_idx: usize = (block_size * i) as usize + (i != 0.0) as usize;
+                let end_idx: usize = (block_size * (i + 1.0)) as usize + 1;
 
-            let (min_index, max_index) = f_argminmax(unsafe {
-                ArrayView1::from_shape_ptr((end_idx - start_idx,), arr.as_ptr().add(start_idx))
-            });
+                let (min_index, max_index) = f_argminmax(&arr[start_idx..end_idx]);
 
-            sampled_index[0] = start_idx;
-            // Add the indexes in sorted order
-            if min_index < max_index {
-                sampled_index[1] = min_index + start_idx;
-                sampled_index[2] = max_index + start_idx;
-            } else {
-                sampled_index[1] = max_index + start_idx;
-                sampled_index[2] = min_index + start_idx;
-            }
-            sampled_index[3] = end_idx - 1;
-        })
+                sampled_index[0] = start_idx;
+                // Add the indexes in sorted order
+                if min_index < max_index {
+                    sampled_index[1] = min_index + start_idx;
+                    sampled_index[2] = max_index + start_idx;
+                } else {
+                    sampled_index[1] = max_index + start_idx;
+                    sampled_index[2] = min_index + start_idx;
+                }
+                sampled_index[3] = end_idx - 1;
+            },
+        )
     };
 
     pool.unwrap().install(zip_func); // allow panic if pool could not be created
@@ -114,17 +113,16 @@ pub(crate) fn m4_generic_parallel<T: Copy + PartialOrd + Send + Sync>(
 
 #[inline(always)]
 pub(crate) fn m4_generic_with_x<T: Copy>(
-    arr: ArrayView1<T>,
+    arr: &[T],
     bin_idx_iterator: impl Iterator<Item = Option<(usize, usize)>>,
     n_out: usize,
-    f_argminmax: fn(ArrayView1<T>) -> (usize, usize),
-) -> Array1<usize> {
+    f_argminmax: fn(&[T]) -> (usize, usize),
+) -> Vec<usize> {
     // Assumes n_out is a multiple of 4
     if n_out >= arr.len() {
-        return Array1::from((0..arr.len()).collect::<Vec<usize>>());
+        return (0..arr.len()).collect::<Vec<usize>>();
     }
 
-    let arr_ptr = arr.as_ptr();
     let mut sampled_indices: Vec<usize> = Vec::with_capacity(n_out);
 
     bin_idx_iterator.for_each(|bin| {
@@ -136,7 +134,7 @@ pub(crate) fn m4_generic_with_x<T: Copy>(
                 }
             } else {
                 // If the bin has > 4 elements, add the first and last + argmin and argmax
-                let step = unsafe { ArrayView1::from_shape_ptr(end - start, arr_ptr.add(start)) };
+                let step = &arr[start..end];
                 let (min_index, max_index) = f_argminmax(step);
 
                 sampled_indices.push(start);
@@ -155,20 +153,20 @@ pub(crate) fn m4_generic_with_x<T: Copy>(
         }
     });
 
-    Array1::from_vec(sampled_indices)
+    sampled_indices
 }
 
 #[inline(always)]
 pub(crate) fn m4_generic_with_x_parallel<T: Copy + PartialOrd + Send + Sync>(
-    arr: ArrayView1<T>,
+    arr: &[T],
     bin_idx_iterator: impl IndexedParallelIterator<Item = impl Iterator<Item = Option<(usize, usize)>>>,
     n_out: usize,
     n_threads: usize,
-    f_argminmax: fn(ArrayView1<T>) -> (usize, usize),
-) -> Array1<usize> {
+    f_argminmax: fn(&[T]) -> (usize, usize),
+) -> Vec<usize> {
     // Assumes n_out is a multiple of 4
     if n_out >= arr.len() {
-        return Array1::from((0..arr.len()).collect::<Vec<usize>>());
+        return (0..arr.len()).collect::<Vec<usize>>();
     }
 
     // to limit the amounts of threads Rayon uses, an explicit threadpool needs to be created
@@ -179,48 +177,41 @@ pub(crate) fn m4_generic_with_x_parallel<T: Copy + PartialOrd + Send + Sync>(
         .build();
 
     let iter_func = || {
-        Array1::from_vec(
-            bin_idx_iterator
-                .flat_map(|bin_idx_iterator| {
-                    bin_idx_iterator
-                        .map(|bin| {
-                            match bin {
-                                Some((start, end)) => {
-                                    if end <= start + 4 {
-                                        // If the bin has <= 4 elements, just return them all
-                                        return (start..end).collect::<Vec<usize>>();
-                                    }
-
-                                    // If the bin has > 4 elements, return the first and last + argmin and argmax
-                                    let step = unsafe {
-                                        ArrayView1::from_shape_ptr(
-                                            end - start,
-                                            arr.as_ptr().add(start),
-                                        )
-                                    };
-                                    let (min_index, max_index) = f_argminmax(step);
-
-                                    // Return the indexes in sorted order
-                                    let mut sampled_index = vec![start, 0, 0, end - 1];
-                                    if min_index < max_index {
-                                        sampled_index[1] = min_index + start;
-                                        sampled_index[2] = max_index + start;
-                                    } else {
-                                        sampled_index[1] = max_index + start;
-                                        sampled_index[2] = min_index + start;
-                                    }
-                                    sampled_index
-                                } // If the bin is empty, return empty Vec
-                                None => {
-                                    vec![]
+        bin_idx_iterator
+            .flat_map(|bin_idx_iterator| {
+                bin_idx_iterator
+                    .map(|bin| {
+                        match bin {
+                            Some((start, end)) => {
+                                if end <= start + 4 {
+                                    // If the bin has <= 4 elements, just return them all
+                                    return (start..end).collect::<Vec<usize>>();
                                 }
+
+                                // If the bin has > 4 elements, return the first and last + argmin and argmax
+                                let step = &arr[start..end];
+                                let (min_index, max_index) = f_argminmax(step);
+
+                                // Return the indexes in sorted order
+                                let mut sampled_index = vec![start, 0, 0, end - 1];
+                                if min_index < max_index {
+                                    sampled_index[1] = min_index + start;
+                                    sampled_index[2] = max_index + start;
+                                } else {
+                                    sampled_index[1] = max_index + start;
+                                    sampled_index[2] = min_index + start;
+                                }
+                                sampled_index
+                            } // If the bin is empty, return empty Vec
+                            None => {
+                                vec![]
                             }
-                        })
-                        .collect::<Vec<Vec<usize>>>()
-                })
-                .flatten()
-                .collect::<Vec<usize>>(),
-        )
+                        }
+                    })
+                    .collect::<Vec<Vec<usize>>>()
+            })
+            .flatten()
+            .collect::<Vec<usize>>()
     };
 
     pool.unwrap().install(iter_func) // allow panic if pool could not be created
