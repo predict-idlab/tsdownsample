4c477059ca6d7e6e3bf1d959138246f7055f87ae
Niels Praet <niels.praet001@gmail.com>
2023-07-31 10:36:05 +0200

    ✅ tests: fix benchmarks

--- downsample_rs/benches/bench_m4.rs
+++ downsample_rs/benches/bench_m4.rs
@@ -7,10 +7,10 @@ fn m4_f32_random_array_long_single_core(c: &mut Criterion) {
     let n = config::ARRAY_LENGTH_LONG;
     let data = utils::get_random_array::<f32>(n, f32::MIN, f32::MAX);
     c.bench_function("m4_scal_f32", |b| {
-        b.iter(|| m4_mod::m4_scalar_without_x(black_box(data.as_slice()), black_box(2_000)))
+        b.iter(|| m4_mod::m4_without_x(black_box(data.as_slice()), black_box(2_000)))
     });
     c.bench_function("m4_simd_f32", |b| {
-        b.iter(|| m4_mod::m4_simd_without_x(black_box(data.as_slice()), black_box(2_000)))
+        b.iter(|| m4_mod::m4_without_x(black_box(data.as_slice()), black_box(2_000)))
     });
 }
 
@@ -20,7 +20,7 @@ fn m4_f32_random_array_long_multi_core(c: &mut Criterion) {
     let all_threads: usize = utils::get_all_threads();
     c.bench_function("m4_scal_p_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_scalar_without_x_parallel(
+            m4_mod::m4_without_x_parallel(
                 black_box(data.as_slice()),
                 black_box(2_000),
                 black_box(all_threads),
@@ -29,7 +29,7 @@ fn m4_f32_random_array_long_multi_core(c: &mut Criterion) {
     });
     c.bench_function("m4_simd_p_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_simd_without_x_parallel(
+            m4_mod::m4_without_x_parallel(
                 black_box(data.as_slice()),
                 black_box(2_000),
                 black_box(all_threads),
@@ -43,14 +43,14 @@ fn m4_f32_random_array_50M_single_core(c: &mut Criterion) {
     let data = utils::get_random_array::<f32>(n, f32::MIN, f32::MAX);
     let x = (0..n).map(|i| i as i32).collect::<Vec<i32>>();
     c.bench_function("m4_scal_50M_f32", |b| {
-        b.iter(|| m4_mod::m4_scalar_without_x(black_box(data.as_slice()), black_box(2_000)))
+        b.iter(|| m4_mod::m4_without_x(black_box(data.as_slice()), black_box(2_000)))
     });
     c.bench_function("m4_simd_50M_f32", |b| {
-        b.iter(|| m4_mod::m4_simd_without_x(black_box(data.as_slice()), black_box(2_000)))
+        b.iter(|| m4_mod::m4_without_x(black_box(data.as_slice()), black_box(2_000)))
     });
     c.bench_function("m4_scalx_50M_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_scalar_with_x(
+            m4_mod::m4_with_x(
                 black_box(x.as_slice()),
                 black_box(data.as_slice()),
                 black_box(2_000),
@@ -59,7 +59,7 @@ fn m4_f32_random_array_50M_single_core(c: &mut Criterion) {
     });
     c.bench_function("m4_simdx_50M_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_simd_with_x(
+            m4_mod::m4_with_x(
                 black_box(x.as_slice()),
                 black_box(data.as_slice()),
                 black_box(2_000),
@@ -75,7 +75,7 @@ fn m4_f32_random_array_50M_multi_core(c: &mut Criterion) {
     let all_threads: usize = utils::get_all_threads();
     c.bench_function("m4_scal_p_50M_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_scalar_without_x_parallel(
+            m4_mod::m4_without_x_parallel(
                 black_box(data.as_slice()),
                 black_box(2_000),
                 black_box(all_threads),
@@ -84,7 +84,7 @@ fn m4_f32_random_array_50M_multi_core(c: &mut Criterion) {
     });
     c.bench_function("m4_simd_p_50M_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_simd_without_x_parallel(
+            m4_mod::m4_without_x_parallel(
                 black_box(data.as_slice()),
                 black_box(2_000),
                 black_box(all_threads),
@@ -93,7 +93,7 @@ fn m4_f32_random_array_50M_multi_core(c: &mut Criterion) {
     });
     c.bench_function("m4_scalx_p_50M_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_scalar_with_x_parallel(
+            m4_mod::m4_with_x_parallel(
                 black_box(x.as_slice()),
                 black_box(data.as_slice()),
                 black_box(2_000),
@@ -103,7 +103,7 @@ fn m4_f32_random_array_50M_multi_core(c: &mut Criterion) {
     });
     c.bench_function("m4_simdx_p_50M_f32", |b| {
         b.iter(|| {
-            m4_mod::m4_simd_with_x_parallel(
+            m4_mod::m4_with_x_parallel(
                 black_box(x.as_slice()),
                 black_box(data.as_slice()),
                 black_box(2_000),
