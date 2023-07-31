f1f5883c1ef03f46804bfe8e173705f7a1749418
Niels Praet <niels.praet001@gmail.com>
2023-07-31 11:33:15 +0200

    ✅ tests: fix benchmarks for minmaxlttb

--- downsample_rs/benches/bench_minmaxlttb.rs
+++ downsample_rs/benches/bench_minmaxlttb.rs
@@ -11,7 +11,7 @@ fn minmaxlttb_f32_random_array_long_single_core(c: &mut Criterion) {
     let y = utils::get_random_array::<f32>(n, f32::MIN, f32::MAX);
     c.bench_function("mmlttb_scalx_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_scalar_with_x(
+            minmaxlttb_mod::minmaxlttb_with_x(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -21,7 +21,7 @@ fn minmaxlttb_f32_random_array_long_single_core(c: &mut Criterion) {
     });
     c.bench_function("mlttb_simdx_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_simd_with_x(
+            minmaxlttb_mod::minmaxlttb_with_x(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -38,7 +38,7 @@ fn minmaxlttb_f32_random_array_long_multi_core(c: &mut Criterion) {
     let all_threads: usize = utils::get_all_threads();
     c.bench_function("mmlttb_scalx_p_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_scalar_with_x_parallel(
+            minmaxlttb_mod::minmaxlttb_with_x_parallel(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -49,7 +49,7 @@ fn minmaxlttb_f32_random_array_long_multi_core(c: &mut Criterion) {
     });
     c.bench_function("mlttb_simdx_p_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_simd_with_x_parallel(
+            minmaxlttb_mod::minmaxlttb_with_x_parallel(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -66,7 +66,7 @@ fn minmaxlttb_f32_random_array_50M_single_core(c: &mut Criterion) {
     let y = utils::get_random_array::<f32>(n, f32::MIN, f32::MAX);
     c.bench_function("mlttb_scalx_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_scalar_with_x(
+            minmaxlttb_mod::minmaxlttb_with_x(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -76,7 +76,7 @@ fn minmaxlttb_f32_random_array_50M_single_core(c: &mut Criterion) {
     });
     c.bench_function("mlttb_simdx_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_simd_with_x(
+            minmaxlttb_mod::minmaxlttb_with_x(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -93,7 +93,7 @@ fn minmaxlttb_f32_random_array_50M_multi_core(c: &mut Criterion) {
     let all_threads: usize = utils::get_all_threads();
     c.bench_function("mlttb_scalx_p_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_scalar_with_x_parallel(
+            minmaxlttb_mod::minmaxlttb_with_x_parallel(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -104,7 +104,7 @@ fn minmaxlttb_f32_random_array_50M_multi_core(c: &mut Criterion) {
     });
     c.bench_function("mlttb_simdx_p_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_simd_with_x_parallel(
+            minmaxlttb_mod::minmaxlttb_with_x_parallel(
                 black_box(x.as_slice()),
                 black_box(y.as_slice()),
                 black_box(2_000),
@@ -120,7 +120,7 @@ fn minmaxlttb_without_x_f32_random_array_50M_single_core(c: &mut Criterion) {
     let y = utils::get_random_array::<f32>(n, f32::MIN, f32::MAX);
     c.bench_function("mlttb_scal_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_scalar_without_x(
+            minmaxlttb_mod::minmaxlttb_without_x(
                 black_box(y.as_slice()),
                 black_box(2_000),
                 black_box(MINMAX_RATIO),
@@ -129,7 +129,7 @@ fn minmaxlttb_without_x_f32_random_array_50M_single_core(c: &mut Criterion) {
     });
     c.bench_function("mlttb_simd_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_simd_without_x(
+            minmaxlttb_mod::minmaxlttb_without_x(
                 black_box(y.as_slice()),
                 black_box(2_000),
                 black_box(MINMAX_RATIO),
@@ -144,7 +144,7 @@ fn minmaxlttb_without_x_f32_random_array_50M_multi_core(c: &mut Criterion) {
     let all_threads: usize = utils::get_all_threads();
     c.bench_function("mlttb_scal_p_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_scalar_without_x_parallel(
+            minmaxlttb_mod::minmaxlttb_without_x_parallel(
                 black_box(y.as_slice()),
                 black_box(2_000),
                 black_box(MINMAX_RATIO),
@@ -154,7 +154,7 @@ fn minmaxlttb_without_x_f32_random_array_50M_multi_core(c: &mut Criterion) {
     });
     c.bench_function("mlttb_simd_p_50M_f32", |b| {
         b.iter(|| {
-            minmaxlttb_mod::minmaxlttb_simd_without_x_parallel(
+            minmaxlttb_mod::minmaxlttb_without_x_parallel(
                 black_box(y.as_slice()),
                 black_box(2_000),
                 black_box(MINMAX_RATIO),
