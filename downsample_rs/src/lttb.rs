6c178e186dac70cea87aa2d523629a8394a8aa5d
Niels Praet <niels.praet001@gmail.com>
2023-09-19 16:46:25 +0200

    💩 refactor: first pass of refactoring code

--- downsample_rs/src/lttb.rs
+++ downsample_rs/src/lttb.rs
@@ -1,4 +1,4 @@
-use super::helpers::Average;
+use super::helpers::LttbParam;
 use super::types::Num;
 use num_traits::AsPrimitive;
 use polars::prelude::*;
@@ -38,51 +38,7 @@ pub fn lttb_with_x<Tx: Num + AsPrimitive<f64>, Ty: Num + AsPrimitive<f64>>(
 
     for i in 0..n_out - 2 {
         // Calculate point average for next bucket (containing c).
-        let avg_range_start = (every * (i + 1) as f64) as usize + 1;
-        let avg_range_end = cmp::min((every * (i + 2) as f64) as usize + 1, x.len());
-
-        let y_slice = &y[avg_range_start..avg_range_end];
-        let avg_y: f64 = y_slice.average();
-        // TODO: avg_y could be approximated argminmax instead of mean?
-        // TODO: below is faster than above, but not as accurate
-        // let avg_x: f64 = (x_slice[avg_range_end - 1].as_() + x_slice[avg_range_start].as_()) / 2.0;
-        let avg_x: f64 = unsafe {
-            (x.get_unchecked(avg_range_end - 1).as_() + x.get_unchecked(avg_range_start).as_())
-                / 2.0
-        };
-
-        // Get the range for this bucket
-        let range_offs = (every * i as f64) as usize + 1;
-        let range_to = avg_range_start; // = start of the next bucket
-
-        // Point a
-        let point_ax = unsafe { x.get_unchecked(a).as_() };
-        let point_ay = unsafe { y.get_unchecked(a).as_() };
-
-        let d1 = point_ax - avg_x;
-        let d2 = avg_y - point_ay;
-        let offset: f64 = d1 * point_ay + d2 * point_ax;
-
-        let x_slice = &x[range_offs..range_to];
-        let y_slice = &y[range_offs..range_to];
-        (_, a) = y_slice.iter().zip(x_slice.iter()).enumerate().fold(
-            (-1i64, a),
-            |(max_area, a), (i, (y_, x_))| {
-                // Calculate triangle area over three buckets
-                // -> area = d1 * (y_ - point_ay) - (point_ax - x_) * d2;
-                // let area = d1 * y[i].as_() + d2 * x[i].as_() - offset;
-                // let area = d1 * y_slice[i].as_() + d2 * x_slice[i].as_() - offset;
-                let area = d1 * y_.as_() + d2 * x_.as_() - offset;
-                let area = f64_to_i64unsigned(area); // this is faster than abs
-                if area > max_area {
-                    (area, i)
-                } else {
-                    (max_area, a)
-                }
-            },
-        );
-        a += range_offs;
-
+        a = lttb_generic(x, y, i, every, a);
         sampled_indices[i + 1] = a;
     }
 
@@ -107,70 +63,13 @@ pub fn lttb_without_x<Ty: Num + AsPrimitive<f64>>(y: &[Ty], n_out: usize) -> Vec
 
     let mut sampled_indices: Vec<usize> = vec![usize::default(); n_out];
 
+    let x = (0..y.len()).map(|v| v.as_()).collect::<Vec<f64>>();
+
     // Always add the first point
     sampled_indices[0] = 0;
 
     for i in 0..n_out - 2 {
-        // Calculate point average for next bucket (containing c).
-        let avg_range_start = (every * (i + 1) as f64) as usize + 1;
-        let avg_range_end = cmp::min((every * (i + 2) as f64) as usize + 1, y.len());
-
-        let y_slice = &y[avg_range_start..avg_range_end];
-        let avg_y: f64 = y_slice.average();
-        let avg_x: f64 = (avg_range_start + avg_range_end - 1) as f64 / 2.0;
-
-        // Get the range for this bucket
-        let range_offs = (every * i as f64) as usize + 1;
-        let range_to = avg_range_start; // = start of the next bucket
-
-        // Point a
-        let point_ay = unsafe { y.get_unchecked(a).as_() };
-        let point_ax = a as f64;
-
-        let d1 = point_ax - avg_x;
-        let d2 = avg_y - point_ay;
-        let point_ax = point_ax - range_offs as f64;
-
-        // let mut max_area = -1i64;
-        let mut ax_x = point_ax; // point_ax - x[i]
-        let offset: f64 = d1 * point_ay;
-
-        // TODO: for some reason is this faster than the loop below -> check if this is true for other devices
-        let y_slice = &y[range_offs..range_to];
-        (_, a) = y_slice
-            .iter()
-            .enumerate()
-            .fold((-1i64, a), |(max_area, a), (i, y)| {
-                // Calculate triangle area over three buckets
-                // -> area: f64 = d1 * y[i].as_() - ax_x * d2;
-                let area: f64 = d1 * y.as_() - ax_x * d2 - offset;
-                let area: i64 = f64_to_i64unsigned(area);
-                ax_x -= 1.0;
-                if area > max_area {
-                    (area, i + range_offs)
-                } else {
-                    (max_area, a)
-                }
-            });
-
-        // let y_slice = unsafe { std::slice::from_raw_parts(y_ptr.add(range_offs), range_to - range_offs) };
-        // (_, a) = y_slice
-        //     .iter()
-        //     .enumerate()
-        //     .fold((-1i64, a), |(max_area, a), (i, y_)| {
-        //         // Calculate triangle area over three buckets
-        //         // -> area: f64 = d1 * y[i].as_() - ax_x * d2;
-        //         let area: f64 = d1 * y_.as_() - ax_x * d2 - offset;
-        //         let area: i64 = f64_to_i64unsigned(area);
-        //         ax_x -= 1.0;
-        //         if area > max_area {
-        //             (area, i)
-        //         } else {
-        //             (max_area, a)
-        //         }
-        //     });
-        // a += range_offs;
-
+        a = lttb_generic::<[f64], [Ty]>(&x, y, i, every, a);
         sampled_indices[i + 1] = a;
     }
 
@@ -219,55 +118,7 @@ where
     sampled_indices[0] = 0;
 
     for i in 0..n_out - 2 {
-        // Calculate point average for next bucket (containing c).
-        let avg_range_start = (every * (i + 1) as f64) as usize + 1;
-        let avg_range_end = cmp::min((every * (i + 2) as f64) as usize + 1, x.len());
-
-        let y_slice = y.slice(avg_range_start as i64, avg_range_end - avg_range_start);
-        // TODO: there is an implementation for mean... but how do we need to type the parameters?
-        let avg_y: f64 = y_slice.mean().unwrap_or(0f64);
-        // TODO: avg_y could be approximated argminmax instead of mean?
-        // TODO: below is faster than above, but not as accurate
-        // let avg_x: f64 = (x_slice[avg_range_end - 1].as_() + x_slice[avg_range_start].as_()) / 2.0;
-        let avg_x: f64 = unsafe {
-            (x.get_unchecked(avg_range_end - 1).unwrap()
-                + x.get_unchecked(avg_range_start).unwrap())
-                / 2.0f64
-        };
-
-        // Get the range for this bucket
-        let range_offs = (every * i as f64) as usize + 1;
-        let range_to = avg_range_start; // = start of the next bucket
-
-        // Point a
-        let point_ax = unsafe { x.get_unchecked(a).unwrap() };
-        let point_ay = unsafe { y.get_unchecked(a).unwrap() };
-
-        let d1 = point_ax - avg_x;
-        let d2 = avg_y - point_ay;
-        let offset: f64 = d1 * point_ay + d2 * point_ax;
-
-        let x_slice = x.slice(range_offs as i64, range_to - range_offs);
-        let y_slice = y.slice(range_offs as i64, range_to - range_offs);
-        (_, a) = y_slice
-            .into_no_null_iter()
-            .zip(x_slice.into_no_null_iter())
-            .enumerate()
-            .fold((-1i64, a), |(max_area, a), (i, (y_, x_))| {
-                // Calculate triangle area over three buckets
-                // -> area = d1 * (y_ - point_ay) - (point_ax - x_) * d2;
-                // let area = d1 * y[i].as_() + d2 * x[i].as_() - offset;
-                // let area = d1 * y_slice[i].as_() + d2 * x_slice[i].as_() - offset;
-                let area = d1 * y_ + d2 * x_ - offset;
-                let area = f64_to_i64unsigned(area); // this is faster than abs
-                if area > max_area {
-                    (area, i)
-                } else {
-                    (max_area, a)
-                }
-            });
-        a += range_offs;
-
+        a = lttb_generic(&x, &y, i, every, a);
         sampled_indices[i + 1] = a;
     }
 
@@ -297,70 +148,13 @@ pub fn lttb_without_x_ca<Ty: PolarsNumericType>(y: &ChunkedArray<Ty>, n_out: usi
 
     let mut sampled_indices: Vec<usize> = vec![usize::default(); n_out];
 
+    let x = (0..y.len()).map(|v| v.as_()).collect::<Vec<f64>>();
+
     // Always add the first point
     sampled_indices[0] = 0;
 
     for i in 0..n_out - 2 {
-        // Calculate point average for next bucket (containing c).
-        let avg_range_start = (every * (i + 1) as f64) as usize + 1;
-        let avg_range_end = cmp::min((every * (i + 2) as f64) as usize + 1, y.len());
-
-        let y_slice = y.slice(avg_range_start as i64, avg_range_end - avg_range_start);
-        let avg_y: f64 = y_slice.mean().unwrap_or(0f64);
-        let avg_x: f64 = (avg_range_start + avg_range_end - 1) as f64 / 2.0;
-
-        // Get the range for this bucket
-        let range_offs = (every * i as f64) as usize + 1;
-        let range_to = avg_range_start; // = start of the next bucket
-
-        // Point a
-        let point_ay = unsafe { y.get_unchecked(a).unwrap() };
-        let point_ax = a as f64;
-
-        let d1 = point_ax - avg_x;
-        let d2 = avg_y - point_ay;
-        let point_ax = point_ax - range_offs as f64;
-
-        // let mut max_area = -1i64;
-        let mut ax_x = point_ax; // point_ax - x[i]
-        let offset: f64 = d1 * point_ay;
-
-        // TODO: for some reason is this faster than the loop below -> check if this is true for other devices
-        let y_slice = y.slice(range_offs as i64, range_to - range_offs);
-        (_, a) =
-            y_slice
-                .into_no_null_iter()
-                .enumerate()
-                .fold((-1i64, a), |(max_area, a), (i, y)| {
-                    // Calculate triangle area over three buckets
-                    // -> area: f64 = d1 * y[i].as_() - ax_x * d2;
-                    let area: f64 = d1 * y - ax_x * d2 - offset;
-                    let area: i64 = f64_to_i64unsigned(area);
-                    ax_x -= 1.0;
-                    if area > max_area {
-                        (area, i + range_offs)
-                    } else {
-                        (max_area, a)
-                    }
-                });
-
-        // let y_slice = unsafe { std::slice::from_raw_parts(y_ptr.add(range_offs), range_to - range_offs) };
-        // (_, a) = y_slice
-        //     .iter()
-        //     .enumerate()
-        //     .fold((-1i64, a), |(max_area, a), (i, y_)| {
-        //         // Calculate triangle area over three buckets
-        //         // -> area: f64 = d1 * y[i].as_() - ax_x * d2;
-        //         let area: f64 = d1 * y_.as_() - ax_x * d2 - offset;
-        //         let area: i64 = f64_to_i64unsigned(area);
-        //         ax_x -= 1.0;
-        //         if area > max_area {
-        //             (area, i)
-        //         } else {
-        //             (max_area, a)
-        //         }
-        //     });
-        // a += range_offs;
+        a = lttb_generic(&x, &y, i, every, a);
 
         sampled_indices[i + 1] = a;
     }
@@ -369,6 +163,63 @@ pub fn lttb_without_x_ca<Ty: PolarsNumericType>(y: &ChunkedArray<Ty>, n_out: usi
     sampled_indices[n_out - 1] = y.len() - 1;
 
     sampled_indices
+}
+
+fn lttb_generic<X: LttbParam + ?Sized, Y: LttbParam + ?Sized>(
+    x: &X,
+    y: &Y,
+    index: usize,
+    every: f64,
+    a: usize,
+) -> usize {
+    // Calculate point average for next bucket (containing c).
+    let avg_range_start = (every * (index + 1) as f64) as usize + 1;
+    let avg_range_end = cmp::min((every * (index + 2) as f64) as usize + 1, x.len());
+
+    let y_slice = y.slice(avg_range_start, avg_range_end);
+    // TODO: there is an implementation for mean... but how do we need to type the parameters?
+    let avg_y: f64 = y_slice.average();
+    // TODO: avg_y could be approximated argminmax instead of mean?
+    // TODO: below is faster than above, but not as accurate
+    // let avg_x: f64 = (x_slice[avg_range_end - 1].as_() + x_slice[avg_range_start].as_()) / 2.0;
+    let avg_x = (x.get(avg_range_end - 1) + x.get(avg_range_start)) / 2.0;
+
+    // Get the range for this bucket
+    let range_offs = (every * index as f64) as usize + 1;
+    let range_to = avg_range_start; // = start of the next bucket
+
+    // Point a
+    let point_ax = x.get(a);
+    let point_ay = y.get(a);
+
+    let d1 = point_ax - avg_x;
+    let d2 = avg_y - point_ay;
+
+    let offset = d1 * point_ay + d2 * point_ax;
+
+    let x_slice = x.slice(range_offs, range_to);
+    let y_slice = y.slice(range_offs, range_to);
+    let mut a = a;
+
+    (_, a) = y_slice
+        .into_iter()
+        .zip(x_slice.into_iter())
+        .enumerate()
+        .fold((-1i64, a), |(max_area, a), (i, (y_, x_))| {
+            // Calculate triangle area over three buckets
+            // -> area = d1 * (y_ - point_ay) - (point_ax - x_) * d2;
+            // let area = d1 * y[i].as_() + d2 * x[i].as_() - offset;
+            // let area = d1 * y_slice[i].as_() + d2 * x_slice[i].as_() - offset;
+            let area = d1 * y_ + d2 * x_ - offset;
+            let area = f64_to_i64unsigned(area); // this is faster than abs
+            if area > max_area {
+                (area, i)
+            } else {
+                (max_area, a)
+            }
+        });
+
+    a + range_offs
 }
 
 // --------------------------------------- TESTS ---------------------------------------
